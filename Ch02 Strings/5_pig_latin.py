a166e4338c9f844de9b7490bfd7e9b731e0b68eb
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-09-04 11:41:39 -0500

    Revert "Add string lesson."
    This reverts commit 2793b95941ba7e668c4416aff4379a3f62e7fbce.

--- Ch02 Strings/5_pig_latin.py
+++ Ch02 Strings/5_pig_latin.py
@@ -1,6 +1,6 @@
 def pigsay(word):
     if word[0].lower() in 'aeiou':
-        return f'{word}way'.title()
-    return f'{word[1:]}{word[0]}ay'.title()
+        return f'{word}way'
+    return f'{word[1:]}{word[0]}ay'
 
-print(pigsay("elements"))
+print(pigsay("samuel"))
